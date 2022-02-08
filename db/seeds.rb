790e142c732cbf3b31eda088dc7f4ac444c41504
dianacg1 <diana.carreno@kellogg.northwestern.edu>
2022-02-08 02:14:50 +0000

    migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+class CreateMovies < ActiveRecord::Migration[7.0]
+    def change
+      create_table :movies do |t|
+        t.string :title
+        t.integer :year_released
+        t.string :rated
+        t.integer :person_id
+      end
+    end
+  end
+
+  class CreatePeople < ActiveRecord::Migration[7.0]
+    def change
+      create_table :people do |t|
+        t.string :name
+      end
+    end
+  end
+  
+  class CreateRoles < ActiveRecord::Migration[7.0]
+    def change
+      create_table :people do |t|
+        t.integer :movie_id
+        t.integer :person_id
+        t.string :character_name
+      end
+    end
+  end
